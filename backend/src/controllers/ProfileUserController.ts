7e88d3e3afdc8605b24e3e4b0b8e1ad4fd6b29e0
JoaoAN2 <joao.an.221203@gmail.com>
2022-02-16 13:46:00 -0300

    Rota de perfil

--- backend/src/controllers/ProfileUserController.ts
+++ backend/src/controllers/ProfileUserController.ts
@@ -3,10 +3,24 @@ import { ProfileUserService } from "../services/ProfileUserService";
 
 class ProfileUserController {
 
+    
     async handle(request: Request, response: Response) {
+        
+        type Profile = {
+            id: string,
+            name: string,
+            email: string,
+            admin: boolean,
+            password?: string,
+            created_at: Date,
+            updated_at: Date
+        };
+
         const { user_id } = request;
         const profileUserService = new ProfileUserService();
-        const user = await profileUserService.execute(user_id);
+        const user:Profile | undefined = await profileUserService.execute(user_id);
+        delete user?.password;
+
         return response.json(user);
     }
 
